f0ed56a9ac173f7abe2014614f18f11c9857421f
grzegorz <grzegorz.morgas@pingen.com>
2023-12-22 15:17:31 +0100

    custom changes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    dispatch(new \App\Jobs\Timeout());
+    dispatch(new \App\Jobs\TimeoutWithoutTransaction());
     return view('welcome');
 });
